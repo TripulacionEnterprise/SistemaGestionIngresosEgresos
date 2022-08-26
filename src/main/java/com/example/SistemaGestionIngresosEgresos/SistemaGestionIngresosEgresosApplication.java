edfa36d103e36d9a05c39ca05256fcb540fafd38
Nicolas Spectrum <nlosada98@gmail.com>
2022-08-25 20:15:28 -0500

    agregando proyecto de spring boot con dependencia

---
+++ src/main/java/com/example/SistemaGestionIngresosEgresos/SistemaGestionIngresosEgresosApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaGestionIngresosEgresos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionIngresosEgresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionIngresosEgresosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SistemaGestionIngresosEgresos/SistemaGestionIngresosEgresosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaGestionIngresosEgresos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionIngresosEgresosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
