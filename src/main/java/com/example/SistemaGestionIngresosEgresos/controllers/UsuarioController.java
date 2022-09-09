f04032896836da87dcd56bfa0b1d0092fd37eb66
Fernando A. Rodriguez R <softfar@gmail.com>
2022-09-09 17:08:34 -0500

    Controlador REST para Usuarios. Incluye los Métodos GET, POST para la ruta /usuarios, y métodos GET, PATCH y DELETE para la ruta /user/{id}

---
+++ src/main/java/com/example/SistemaGestionIngresosEgresos/controllers/UsuarioController.java
@@ -0,0 +1,4 @@
+package com.example.SistemaGestionIngresosEgresos.controllers;
+
+public class UsuarioController {
+}
---
+++ src/main/java/com/example/SistemaGestionIngresosEgresos/respositories/IUsuarioRepository.java
@@ -0,0 +1,2 @@
+package com.example.SistemaGestionIngresosEgresos.respositories;public class IUsuarioRepository {
+}
